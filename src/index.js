3abaf1f2c45d48c744e98057de207b37c2fa8cdb
Zhakhsylyk <57891381+Zhakhsylyk@users.noreply.github.com>
2022-07-02 16:52:51 +0600

    media deploy

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { store, persistor } from "./store/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
